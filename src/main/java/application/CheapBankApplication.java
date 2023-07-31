e34157a367754a6c712c6bfa9d759fae8676149e
RGabriel26 <gabrielrosu112@gmail.com>
2023-07-31 23:59:49 +0300

    test commit from eclipse

--- src/main/java/application/CheapBankApplication.java
+++ src/main/java/application/CheapBankApplication.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("cp.entity")
 @SpringBootApplication(scanBasePackages = {"cp.bank.controller","cp.tempclass","services"})
 public class CheapBankApplication {
-	
+
 	public static Long loggedID;
 	
 	public static void main(String[] args) {
