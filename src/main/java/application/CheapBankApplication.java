68a63d826d0a528aec29a5879b04048e0decdc9c
GBRielRG <gabrielrosu112@gmail.com>
2023-07-31 23:51:08 +0300

    test commit from eclipe

--- src/main/java/application/CheapBankApplication.java
+++ src/main/java/application/CheapBankApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
-//@ComponentScan(basePackages = {"cp.login","cp.deshboard","cp.tempclass","services"})
 @EnableJpaRepositories(basePackages = "repo")
 @EntityScan("cp.entity")
 @SpringBootApplication(scanBasePackages = {"cp.bank.controller","cp.tempclass","services"})
